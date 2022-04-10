a54bcb343802c133db3263bcceaddaa3f6ff1b52
Jimin Kim <0305jimin@gmail.com>
2022-04-10 15:25:34 +0900

    2577

--- 10950.py
+++ 10950.py
@@ -3,6 +3,7 @@ for i in range(n):
     a,b=map(int,input().split())
     print(a+b)
 
+# 여러 줄(정해진 개수 O)에 줄마다 여러 숫자 주어질 경우.
 # 어떤 부분이 반복되는가 공통 부분 제대로 알아차리기. 
 # 시간 단축해야 하는 문제 ) import sys 통해 sys.stdin.readline() 사용
 # for i in range(n) 내부에서 i 응용 출력 형태 주의. 범위의 경계 체크. 
--- 10951.py
+++ 10951.py
@@ -5,6 +5,6 @@ for line in lines:
     a,b=map(int,line.split())
     print(a+b)
     
-# 여러 줄에 줄마다 여러 숫자 주어질 경우 :
+# 여러 줄(정해진 개수 X)에 줄마다 여러 숫자 주어질 경우 :
     # lines를 일단 다 저장하고 
     # line 별로 map으로 숫자 끊어 저장. 
