6920631b890ed1fcb431437f4026aa8e167a4450
Jimin Kim <0305jimin@gmail.com>
2022-04-21 13:11:50 +0900

    refactor ; 2941

--- 2941.py
+++ 2941.py
@@ -1,11 +1,5 @@
 input=input()
 croatian=['c=','c-','dz=','d-','lj','nj','s=','z=']
-count=0
 for alpha in croatian:
-    if alpha in input:
-        count+=1
-        input=input.replace(alpha,'*',1)
-for char in input:
-    if char.isalpha():
-        count+=1
-print(count)
+    input=input.replace(alpha,'*')
+print(len(input))
