f9d9d0f4b7531e122edd1b968a686c43904f9921
Jimin Kim <0305jimin@gmail.com>
2022-04-24 12:44:10 +0900

    11729 ; towers of hanoi

--- 11729.py
+++ 11729.py
@@ -1,16 +1,16 @@
 n=int(input())
-def hanoii_num(n):
+def hanoi_num(n):
     if n==1:
         return n
     if n==2:
         return 3
-    return hanoii_num(n-1)+1+hanoii_num(n-1)
-def hanoii_sequence(n,src,dest,temp):
+    return hanoi_num(n-1)+1+hanoi_num(n-1)
+def hanoi_sequence(n,src,dest,temp):
     if n==1:
         print(src,dest)
     else:
-        hanoii_sequence(n-1,src,temp,dest)
-        hanoii_sequence(1,src,dest,temp)
-        hanoii_sequence(n-1,temp,dest,src)
-print(hanoii_num(n))
-hanoii_sequence(n,1,3,2)
+        hanoi_sequence(n-1,src,temp,dest)
+        hanoi_sequence(1,src,dest,temp)
+        hanoi_sequence(n-1,temp,dest,src)
+print(hanoi_num(n))
+hanoi_sequence(n,1,3,2)
