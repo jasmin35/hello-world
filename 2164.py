694313e04b11479a3e8002695922e18da51b1467
Jimin Kim <0305jimin@gmail.com>
2022-04-24 15:18:17 +0900

    2164

---
+++ 2164.py
@@ -0,0 +1,9 @@
+import sys
+from collections import deque
+
+n=int(sys.stdin.readline())
+queue=deque(list(range(1,n+1)))
+while len(queue)!=1 :
+    queue.popleft()
+    queue.append(queue.popleft())
+print(queue[0])
