6dfaab82c17233875c02287280c624646cfa88ce
Jimin Kim <0305jimin@gmail.com>
2022-04-27 23:35:17 +0900

    9093

---
+++ 9093.py
@@ -0,0 +1,14 @@
+t=int(input())
+for i in range(t):
+    sentence=input()
+    stack=[]
+    for char in sentence:
+        if char==" ":
+            while stack:
+                print(stack.pop(),end="")
+            print(" ",end="")
+        else:
+            stack.append(char)
+    while stack:
+        popped=stack.pop()
+        print(popped,end="")
