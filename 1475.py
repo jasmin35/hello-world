3fafc349294e298e1ca2b59fa4965985693aecfc
Jimin Kim <0305jimin@gmail.com>
2022-06-17 19:14:11 +0900

    Feat : 1475

--- 1475.py
+++ 1475.py
@@ -6,8 +6,15 @@ for n in num :
     if n=='9'or n=='6':
         six+=1
     else:
-        normal[f'{n}']+=1
-    print(normal)
-min_num = min(normal.values())
+        if n in normal :
+            previous=normal[f'{n}']
+        else :
+            previous=0
+        normal[f'{n}']=previous+1
+    # print(normal,six)
+if normal :
+    min_num = max(normal.values())
+else:
+    min_num =0
 min_num_2=math.ceil(six/2)
-print(min(min_num,min_num_2))
+print(max(min_num,min_num_2))
