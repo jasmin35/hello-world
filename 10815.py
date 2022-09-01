cb9e8a51fd4bd603e6d83c72406bc3bd50e1077a
Jimin Kim <0305jimin@gmail.com>
2022-09-01 21:42:39 +0900

    Refactor: 10815

--- 10815.py
+++ 10815.py
@@ -1,5 +1,8 @@
 num_of_criteria = int(input())
-criteria = list(map(int, input().split()))
+criteria = set(map(int, input().split()))
+# https://wiki.python.org/moin/TimeComplexity
+# "list에서 in을 수행하면 모든 요소를 처음부터 검사하면서 찾고자 하는 원소가 있는지 확인하지만, 
+# set은 해시로 구현되어 있으므로 x in set이 x in list보다 일반적으로 더 빨리 동작하게 됩니다."
 num_of_given = int(input())
 given_nums = list(map(int, input().split()))
 
