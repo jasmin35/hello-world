a90010ce2fdd38dafb2d9aa5f4eb74f107ef2b74
Jimin Kim <0305jimin@gmail.com>
2022-09-06 18:42:40 +0900

    Feat: 05

--- 05.py
+++ 05.py
@@ -1,14 +1,19 @@
-# prime_list는 1부터 10000사이의 소수가 오름차순으로 저장된 리스트예요.
-from prime import prime_list
+prime_list = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53]
 
 # 합계가 짝수가 되는 두 소수를 찾는 함수예요.
 def goldbach(arr):
+    array = []
     # 합계가 짝수가 되는 두 소수를 작은 수부터 차례로 리스트에 저장해 주세요.
-    for even_num, index in enumerate(arr):
+    # print(arr)
+    for even_num in arr:
+        # print("came in")
         for i in range(even_num):
+            # print(i)
             if i in prime_list and even_num-i in prime_list:
-                arr[index]=[i,even_num-i]
-    return None
+                array.append([i,even_num-i])
+                break
+                # print(array)
+    return array
 
 
 arr = [int(x) for x in input().split()] # 입력된 n개의 짝수가 리스트에 저장된다.
