3bcddb0d7749a7778370697872e9d339eb2b623b
Jimin Kim <0305jimin@gmail.com>
2022-09-20 16:32:43 +0900

    9095
    순열의 상황 적용, 문제 조건 반영

--- BOJ/9095.py
+++ BOJ/9095.py
@@ -1,15 +1,19 @@
 the_number_of_cases = int(input())
 
-results = []
+results_to_print = []
+
+# 문제 조건에서 합의 기준으로 주어딜 숫자가 11이하의 양수라고 했으므로 하드코딩 가능. 
+dp = [0]*12
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+for j in range(4,12):
+    dp[j] = dp[j-1] + dp[j-2] + dp[j-3]
+
 # the_number_of_cases만큼 입력받고 계산하고 출력할 값 저장
 for i in range(the_number_of_cases):
     goal = int(input())
-    dp = [0]*(goal+1)
-    dp[0] = 1
-    for value in range(1,4):
-        for j in range(value,goal+1):
-            dp[j] += dp[j-value]
-    results.append(dp[goal])
+    results_to_print.append(dp[goal])
 
-for result in results:
+for result in results_to_print:
     print(result)
